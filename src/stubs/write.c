fc69dae849a99a8a4e32120adaf090ad392e8159
fAiL-ix <14235595+fAiL-ix@users.noreply.github.com>
2020-12-26 19:13:26 +0100

    added write stub

---
+++ src/stubs/write.c
@@ -0,0 +1,17 @@
+/* See LICENSE of license details. */
+
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+#include <gd32vf103.h>
+
+#if   defined (__GNUC__)
+#include <unistd.h>
+#include <sys/types.h>
+#endif
+
+#include "stub.h"
+
+ssize_t _write(int fd, const void* ptr, size_t len) {
+    return _stub(EBADF);
+}
